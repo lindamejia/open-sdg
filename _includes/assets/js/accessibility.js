4b80c12a052722782c794f0f1d9078da3a6a4013
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2019-08-21 14:03:41 +0100

    Update accessibility.js

--- _includes/assets/js/accessibility.js
+++ _includes/assets/js/accessibility.js
@@ -74,16 +74,18 @@ var accessibilitySwitcher = function() {
   
   
 function getContrastToggleLabel(identifier){
-  {%- if site.contrast_type === 'short' -%} 
+  var contrast_type= site.contrast_type
+  if(contrast_type === 'short') { 
     return 'A'
-  {%- elsif site.contrast_type === 'long' -%}
+  }
+  else if(contrast_type === 'long') {
     if(identifier === "default"){	
       return translations.header.default_contrast; 	
     }	
     else if(identifier === "high"){	
       return translations.header.high_contrast;	
     }
-  {%- endif -%}
+  }
 }
 
 function getContrastToggleTitle(identifier){	
