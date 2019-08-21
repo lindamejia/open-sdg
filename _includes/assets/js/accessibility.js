b2c75fdfc303cc7607304afc27ce58f33ca306c0
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2019-08-21 14:39:49 +0100

    Update accessibility.js

--- _includes/assets/js/accessibility.js
+++ _includes/assets/js/accessibility.js
@@ -74,17 +74,17 @@ var accessibilitySwitcher = function() {
   
   
 function getContrastToggleLabel(identifier){
-  var contrast_type = "+{{ site.contrast_type }}+"
-  if(contrast_type === "short") { 
-    return 'A'
-  }
-  else if(contrast_type === "long") {
+  var contrast_type = "{{ site.contrast_type }}"
+  if(contrast_type === "long") {
     if(identifier === "default"){	
       return translations.header.default_contrast; 	
     }	
     else if(identifier === "high"){	
       return translations.header.high_contrast;	
     }
+  }
+  else {
+    return 'A'
   }
 }
 
